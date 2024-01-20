697bc73ff44ba0619f3c8b854d685814584d9caf
Kamalini Pradhan <156031848+S573647@users.noreply.github.com>
2024-01-19 23:23:23 -0600

    Add files via upload

---
+++ start_project.py
@@ -0,0 +1,17 @@
+""" Start a data analytics project. """
+import pathlib
+
+def create_project_directory(directory_name: str):
+    """
+    Create a project sub-directory.
+    :param directory_name: Name to be created, e.g. "test"
+    """
+    pathlib.Path(directory_name).mkdir(exist_ok=True)
+
+def main():
+    """ Scaffold a project. """
+    create_project_directory(directory_name='test')
+    create_project_directory(directory_name='docs')
+
+if __name__== '__main__':
+    main()
